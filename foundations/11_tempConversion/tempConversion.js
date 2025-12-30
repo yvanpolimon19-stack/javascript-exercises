dc5fd85abdf103d54cb5899540e3a6807fdf0a2a
yvanpolimon19-stack <yvanpolimon19@gmail.com>
2025-12-30 22:52:53 +0400

    k

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempf) {
+  tempf = (tempf - 32) * (5/9);
+  tempf = tempf.toFixed(2);
+  tempf = parseFloat(tempf);
+  return tempf
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempc) {
+
+  tempc = (tempc * (9/5)) + 32;
+  tempc = tempc.toFixed(2);
+  tempc = parseFloat(tempc);
+  return tempc
+
 };
 
 // Do not edit below this line
