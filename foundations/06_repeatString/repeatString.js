e2eca081065f139b162945fa2f9ac28b157e972b
yvanpolimon19-stack <yvanpolimon19@gmail.com>
2025-12-30 22:03:00 +0400

    had some difficulties, tried to use * like in python

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string,number) {
+    let arr = "";
+    for(let i = 1; i <=number; i++ ){
+        arr = arr + string
+    }
+    return arr
 };
 
 // Do not edit below this line
