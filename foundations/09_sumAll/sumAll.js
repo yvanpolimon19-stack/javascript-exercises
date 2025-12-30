8e02569ddaa64f05793c85b4f0b7efae0e598c16
yvanpolimon19-stack <yvanpolimon19@gmail.com>
2025-12-30 22:26:59 +0400

    k

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(lower,higher) {
+    let sum = 0;
+    for(let i = lower; i = higher; i++){
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
