9e26eb0d97320a012cf3af3b49186c08e5198af3
yvanpolimon19-stack <yvanpolimon19@gmail.com>
2025-12-30 22:20:54 +0400

    i just used the filter method should try it later with other array methods

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+
+const removeFromArray = function(arr,number1,number2,number3) {
+    arr = arr.filter(num =>(num !== number1 && num !== number2 && num !== number3));
+    return arr;
+
 };
 
 // Do not edit below this line
