603b40e81e3bd03785aa448069d3ad0ea5bfb152
yvanpolimon19-stack <yvanpolimon19@gmail.com>
2025-12-30 22:08:01 +0400

    This one was pretty simple and straight forward loops may be better in js than python

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newStr = ""
+    for(let i = string.length - 1; i >= 0; i--){
+        newStr += string[i]
+    }
 
+    return newStr
 };
 
 // Do not edit below this line
