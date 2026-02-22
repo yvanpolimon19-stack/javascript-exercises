15f80151ff26c2fccaa33a170f6b4d4abdca21b0
Yvan Polimon <yvanpolimon19@gmail.com>
2026-02-22 19:30:47 +0400

    was not that hard

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,28 @@
-const findTheOldest = function() {
+
+const findTheOldest = function(array) {
+
+    const currentYear = new Date().getFullYear();
+    let greatestAge = 0
+    let oldest = array[0];
+    let age;
+
+    for(var i = 0; i < array.length; i++){
+
+        if ("yearOfDeath" in array[i]) {
+            age = array[i].yearOfDeath - array[i].yearOfBirth;
+        }
+
+        else {
+            age = currentYear - array[i].yearOfBirth;
+        }
+
+        if (age > greatestAge) {
+            greatestAge = age;
+            oldest = array[i];
+        }
+            
+    }
+    return oldest;
 
 };
 
